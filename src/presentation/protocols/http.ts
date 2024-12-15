9e874d25a6db1896b08666700b177e32b5a23576
guilhermelr291 <guilhermelr291dev@gmail.com>
2024-12-15 14:56:07 -0300

    refactor: ensure SignUpController.handle implements correct interfaces

---
+++ src/presentation/protocols/http.ts
@@ -0,0 +1,8 @@
+export interface HttpResponse {
+  statusCode: number;
+  body: any; //pode ser qualquer tipo de objetivo, por isso o any.
+}
+
+export interface HttpRequest {
+  body?: any; //o body é opcional, pois o get, por ex, n usa o body e sim req.params, query, etc..
+}
