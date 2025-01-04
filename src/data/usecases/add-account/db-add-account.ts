cbe4df25cf4643d746a5d6ca23542b6f1ba39ce8
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-01-04 20:23:57 -0300

    feat: ensure DbAddAccountRepository calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,16 +2,25 @@ import {
   AccountModel,
   AddAccount,
   AddAccountModel,
+  AddAccountRepository,
   Encrypter,
 } from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
-  constructor(encrypter: Encrypter) {
+  private readonly addAccountRepository: AddAccountRepository;
+  constructor(
+    encrypter: Encrypter,
+    addAccountRepository: AddAccountRepository
+  ) {
     this.encrypter = encrypter;
+    this.addAccountRepository = addAccountRepository;
   }
-  async add(account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password);
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password); //IMPORTANT vale lembrar que o accountData que recebemos no parametro nao ta com a senha criptografada. se passarmos para o repositorio, vai dar erro.
+    await this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: hashedPassword })
+    );
     return new Promise(resolve => resolve(null));
   }
 }
