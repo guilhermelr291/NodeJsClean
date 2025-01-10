879aa74fd60a7250bdd17c4d298d34a00c16b9c1
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-01-10 17:40:41 -0300

    feat: ensure bcrypt adapter calls bcrypt with correct values

--- src/data/protocols/encrypter.ts
+++ src/data/protocols/encrypter.ts
@@ -1,3 +1,5 @@
 export interface Encrypter {
   encrypt(value: string): Promise<string>;
+  //vale reparar que nao usamos async na assinatura da interface,
+  // somente na implementação do método.
 }
