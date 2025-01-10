6fbdee5f7890cb879bc99c179dc20577516c4c52
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-01-10 17:47:59 -0300

    test: ensure bcrypt adapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise(resolve => resolve('hash'));
+  },
+}));
+
 describe('BcryptAdapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const salt = 12;
@@ -8,5 +14,11 @@ describe('BcryptAdapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value'); //como estamos testando a integração aqui, não estamos preocupados com o retorno.
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+  test('Should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hash = await sut.encrypt('any_value'); //o certo aq é mockarmos o bcrypt. nao nos interessa saber como o bcrypt gera a hash. o que nos interessa é que o valor retornado pelo bcrypt seja igual ao retorno da função encrypt.
+    expect(hash).toBe('hash');
   });
 });
