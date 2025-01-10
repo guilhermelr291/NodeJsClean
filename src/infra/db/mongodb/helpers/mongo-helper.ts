4e83eb2b072ab7b2d9e634d08a36d0448b56a3de
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-01-10 18:50:45 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+export const MongoHelper = {
+  // client: MongoClient, aqui tem um conflito. dessa forma, em um obj,
+  // estamos atribuindo um valor e nao definindo o tipo. temos q declarar
+  // e depois fazer o cast
+  client: null as MongoClient,
+
+  async connect(uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL); //url setada pelo jest-mongodb no
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close();
+  },
+};
