3ce5c3857cdf49701987c07d1f61379ad80eaba5
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-02-06 23:12:32 -0300

    feat: ensure mongodb reconnects if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -5,16 +5,23 @@ export const MongoHelper = {
   // estamos atribuindo um valor e nao definindo o tipo. temos q declarar
   // e depois fazer o cast
   client: null as MongoClient,
+  uri: null as string,
 
   async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL); //url setada pelo jest-mongodb no
+    this.client = await MongoClient.connect(uri); //url setada pelo jest-mongodb no
+    this.uri = uri;
   },
 
   async disconnect(): Promise<void> {
     await this.client.close();
+    this.client = null;
   },
 
-  getCollection(name: string): Collection {
+  //IMPORTANT uma boa prática é sempre verificarmos se o Client está desconectado ou nulo e, se estiver, reconectar. Isso evita erros.
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.topology?.isConnected()) {
+      await this.connect(this.uri);
+    }
     return this.client.db().collection(name);
   },
 };
