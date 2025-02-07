fa08f869ff8822c2b7fcea2150480e659eedaa41
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-02-07 18:35:20 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from '../../presentation/protocols';
+
+//Aqui, vamos aplicar o decorator. O padrão se resume em pegar um objeto e criar um wrapper em volta dele.
+//Dessa forma, não estaremos acoplando o Logger a nenhum controller. Como nao é um use-case, não tem necessidade de acoplarmos.
+//o decorator precisa ter a mesma assinatura da classe que vamos decorar. Como o SignUp Controller implementa a interface Controller, basta implementarmos também.
+// a classe que vamos decorar precisa ser do mesmo tipo da classe que estamos implementando.
+// recebemos a classe que vamos decorar no construtor.
+//agora, sempre que criarmos uma factory para um controller, em vez de retornar o controller, vamos retornar o decorator englobando esse controller.
+//como o decorator tem comportamento, vale a pena criar um teste pra ele, diferente da factory q n tem.
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller;
+  constructor(controller: Controller) {
+    this.controller = controller;
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest); //nosso controller retorna um ok() um ou ServerError(). Se retornar um serverError, podemos fazer um log aqui mesmo, preservando o comportamento da classe original.
+    if (httpResponse.statusCode === 500) {
+      //log
+    }
+    return httpResponse;
+  }
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,14 +3,32 @@ import { SignUpController } from '../../presentation/controllers/signup/signup';
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from '../../presentation/protocols';
+import { LogControllerDecorator } from '../decorators/log';
 
-export const makeSignUpController = (): SignUpController => {
+//agora, em vez de retornamos um signUpController, podemos retornar um LogControllerDecorator, pois eles tem a mesma implementação. Ambos implementam Controller
+//agora, basta invocarmos o handle do controller recebido no LogControllerDecorator.
+//outro principio do solid, que é o do liskov: podemos utilizar uma classe no lugar da outra contanto que ela implemente a mesma interface.
+// vamos mudar a assinatura de retorno de SignUpController para Controller. Precisamos ser mais genéricos agora.
+// o decorator nos permite adicionar um comportamento a um objeto sem modificar o objeto.
+export const makeSignUpController = (): Controller => {
   const salt = 12;
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const bcryptAdapter = new BcryptAdapter(salt);
   const accountMongoRepository = new AccountMongoRepository();
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
-  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+
+  //return new SignUpController(emailValidatorAdapter, dbAddAccount);
+  const signUpController = new SignUpController(
+    emailValidatorAdapter,
+    dbAddAccount
+  );
+
+  return new LogControllerDecorator(signUpController);
   //estamos respeitando o single principle do solid, por isso tantos
   //  componentes para montar o controller
 };
