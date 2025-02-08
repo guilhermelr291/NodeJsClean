b3938425cd13b6331cbaa25f1538b27a6fa78150
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-02-08 18:31:57 -0300

    refactor: remove duplicated code

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -6,8 +6,9 @@ import {
 } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log';
 import { ServerError } from '../../presentation/errors';
-import { serverError } from '../../presentation/helpers/http-helper';
+import { ok, serverError } from '../../presentation/helpers/http-helper';
 import { LogErrorRepository } from '../../data/protocols/log-error-repository';
+import { AccountModel } from '../../domain/models/account';
 
 //como o LogControllerDecorator pode receber qualquer controller, vms criar um controller mockado aqui. um Stub
 
@@ -26,17 +27,34 @@ const makeLogErrorRepository = (): LogErrorRepository => {
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
     handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-      const httpResponse: HttpResponse = {
-        statusCode: 200,
-        body: {
-          name: 'Rodrigo',
-        },
-      };
-      return new Promise(resolve => resolve(httpResponse));
+      return new Promise(resolve => resolve(ok(makeFakeAccount())));
     }
   }
 
   return new ControllerStub();
+};
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password',
+  },
+});
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password',
+});
+
+const makeFakeServerError = (): HttpResponse => {
+  const fakeError = new Error();
+  fakeError.stack = 'any_stack';
+  const error = serverError(fakeError);
+
+  return error;
 };
 interface SutTypes {
   sut: LogControllerDecorator;
@@ -61,55 +79,29 @@ describe('LogControllerDecorator', () => {
     const { sut, controllerStub } = makeSut();
 
     const handleSpy = jest.spyOn(controllerStub, 'handle');
-    const httpRequest = {
-      body: {
-        email: 'any_mail@mail.com',
-        name: 'any_name@name.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    await sut.handle(httpRequest);
-    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+
+    await sut.handle(makeFakeRequest());
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest());
   });
 
   test('Should return the same result of controller', async () => {
     const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        email: 'any_mail@mail.com',
-        name: 'any_name@name.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse).toEqual({
-      statusCode: 200,
-      body: {
-        name: 'Rodrigo',
-      },
-    });
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(ok(makeFakeAccount()));
   });
 
   test('Should call LogErrorRepository with correct error if controller returns a serverError', async () => {
     const { sut, controllerStub, LogErrorRepositoryStub } = makeSut();
-    const fakeError = new Error();
-    fakeError.stack = 'any_stack';
-    const error = serverError(fakeError);
+
     const logSpy = jest.spyOn(LogErrorRepositoryStub, 'log');
     jest
       .spyOn(controllerStub, 'handle')
-      .mockReturnValueOnce(new Promise(resolve => resolve(error))); //como é assíncrono, temos que mockar com Promise. Cometi o erro de retornar uma promise com reject. Nao posso fazer isso, pois assim ele vai lançar uma exceção e quebrar o teste. O que ele retorna no catch n é considerado exceção, mas sim um retorno.
-    const httpRequest = {
-      body: {
-        email: 'any_mail@mail.com',
-        name: 'any_name@name.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    await sut.handle(httpRequest);
+      .mockReturnValueOnce(
+        new Promise(resolve => resolve(makeFakeServerError()))
+      ); //como é assíncrono, temos que mockar com Promise. Cometi o erro de retornar uma promise com reject. Nao posso fazer isso, pois assim ele vai lançar uma exceção e quebrar o teste. O que ele retorna no catch n é considerado exceção, mas sim um retorno.
+
+    await sut.handle(makeFakeRequest());
     expect(logSpy).toHaveBeenCalledWith('any_stack');
   });
 });
