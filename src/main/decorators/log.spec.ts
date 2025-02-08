4c6e57a669e2d17c98ae30211a4c9f8ac65772cb
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-02-08 17:11:15 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,16 +1,27 @@
+import { resolve } from 'path';
 import {
   Controller,
   HttpRequest,
   HttpResponse,
 } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log';
+import { ServerError } from '../../presentation/errors';
+import { serverError } from '../../presentation/helpers/http-helper';
+import { LogErrorRepository } from '../../data/protocols/log-error-repository';
 
 //como o LogControllerDecorator pode receber qualquer controller, vms criar um controller mockado aqui. um Stub
 
-interface SutTypes {
-  sut: LogControllerDecorator;
-  controllerStub: Controller;
-}
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    log(stack: string): Promise<void> {
+      //Promise<void> pq n retorna nd, mas, como lida com o bd, é async.
+
+      return new Promise(resolve => resolve());
+    }
+  }
+
+  return new LogErrorRepositoryStub();
+};
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
@@ -27,12 +38,21 @@ const makeController = (): Controller => {
 
   return new ControllerStub();
 };
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+  LogErrorRepositoryStub: LogErrorRepository;
+}
 
 const makeSut = (): SutTypes => {
   const controllerStub = makeController();
-  const sut = new LogControllerDecorator(controllerStub);
+  const LogErrorRepositoryStub = makeLogErrorRepository();
+  const sut = new LogControllerDecorator(
+    controllerStub,
+    LogErrorRepositoryStub
+  );
 
-  return { sut, controllerStub };
+  return { sut, controllerStub, LogErrorRepositoryStub };
 };
 
 describe('LogControllerDecorator', () => {
@@ -70,5 +90,26 @@ describe('LogControllerDecorator', () => {
         name: 'Rodrigo',
       },
     });
+  });
+
+  test('Should call LogErrorRepository with correct error if controller returns a serverError', async () => {
+    const { sut, controllerStub, LogErrorRepositoryStub } = makeSut();
+    const fakeError = new Error();
+    fakeError.stack = 'any_stack';
+    const error = serverError(fakeError);
+    const logSpy = jest.spyOn(LogErrorRepositoryStub, 'log');
+    jest
+      .spyOn(controllerStub, 'handle')
+      .mockReturnValueOnce(new Promise(resolve => resolve(error))); //como é assíncrono, temos que mockar com Promise. Cometi o erro de retornar uma promise com reject. Nao posso fazer isso, pois assim ele vai lançar uma exceção e quebrar o teste. O que ele retorna no catch n é considerado exceção, mas sim um retorno.
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name@name.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(logSpy).toHaveBeenCalledWith('any_stack');
   });
 });
