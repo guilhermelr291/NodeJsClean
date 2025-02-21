1256108104870d390f74385723081cb749b60e0e
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-02-20 22:23:14 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,5 +1,5 @@
 export * from '../../../domain/models/account';
 
 export * from '../../../domain/usecases/add-account';
-export * from '../../protocols/criptography/encrypter';
+export * from '../../protocols/cryptography/encrypter';
 export * from '../../protocols/db/add-account-repository';
