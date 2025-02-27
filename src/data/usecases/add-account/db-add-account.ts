496dc5ad201270923c12289979b700db52390c47
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-02-27 19:41:15 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -4,18 +4,23 @@ import {
   AddAccountModel,
   AddAccountRepository,
   Hasher,
+  LoadAccountByEmailRepository,
 } from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   constructor(
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {
     this.hasher = hasher;
     this.addAccountRepository = addAccountRepository;
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository;
   }
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
+
     const hashedPassword = await this.hasher.hash(accountData.password); //IMPORTANT vale lembrar que o accountData que recebemos no parametro nao ta com a senha criptografada. se passarmos para o repositorio, vai dar erro.
     const account = await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword })
