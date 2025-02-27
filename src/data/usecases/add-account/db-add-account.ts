936fb0276a4c0d035749d9ed04324c65b20cbfd5
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-02-27 19:51:02 -0300

    feat: ensure DbAddAccount returns null if LoadAccountByEmailRepository returns an account

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -19,13 +19,17 @@ export class DbAddAccount implements AddAccount {
   }
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
-
-    const hashedPassword = await this.hasher.hash(accountData.password); //IMPORTANT vale lembrar que o accountData que recebemos no parametro nao ta com a senha criptografada. se passarmos para o repositorio, vai dar erro.
-    const account = await this.addAccountRepository.add(
-      Object.assign({}, accountData, { password: hashedPassword })
+    const account = await this.loadAccountByEmailRepository.loadByEmail(
+      accountData.email
     );
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password); //IMPORTANT vale lembrar que o accountData que recebemos no parametro nao ta com a senha criptografada. se passarmos para o repositorio, vai dar erro.
+      const newAccount = await this.addAccountRepository.add(
+        Object.assign({}, accountData, { password: hashedPassword })
+      );
 
-    return new Promise(resolve => resolve(account));
+      return newAccount;
+    }
+    return null;
   }
 }
