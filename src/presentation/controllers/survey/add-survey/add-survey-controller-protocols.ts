c3efd6c40ee5638c26e9d85345d078edb6b6c6a4
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-01 15:10:04 -0300

    feat: ensure AddSurveyController calls Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -0,0 +1,2 @@
+//aqui vamos deixar todos os exports dos protocols genéricos mas também poderemos adicionar os exclusivos do SignUpController, diferente do index.js da pasta protocols.
+export * from '../../../protocols';
