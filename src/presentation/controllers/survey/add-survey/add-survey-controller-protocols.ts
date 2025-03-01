80054200a4ebb5a036c353a1062c8d30acfd233e
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-01 17:55:22 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct valuesaddSurvey

--- src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,2 +1,2 @@
-//aqui vamos deixar todos os exports dos protocols genéricos mas também poderemos adicionar os exclusivos do SignUpController, diferente do index.js da pasta protocols.
 export * from '../../../protocols';
+export * from '../../../../domain/usecases/add-survey';
