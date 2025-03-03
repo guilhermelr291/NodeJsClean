ec916e0e9616d59787424ac637e2ea1d4b2e342b
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-03 20:43:48 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,12 +1,18 @@
+import { Decrypter } from '../../../data/protocols/cryptography/decrypter';
 import { Encrypter } from '../../../data/protocols/cryptography/encrypter';
 import jwt from 'jsonwebtoken';
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor(private readonly secret: string) {
     this.secret = secret;
   }
   async encrypt(value: string): Promise<string> {
     const token = jwt.sign({ id: value }, this.secret);
     return token;
+  }
+
+  async decrypt(value: string): Promise<string | null> {
+    jwt.verify(value, this.secret);
+    return null;
   }
 }
