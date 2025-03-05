f212e8d059d07deac0da65ee960a8686f3813f14
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-05 18:31:25 -0300

    feat: ensure add survey route returns 403 if no access token is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { HttpRequest, Middleware } from '../../presentation/protocols';
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers,
+    };
+    const httpResponse = await middleware.handle(httpRequest);
+
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body); //vamos colocar dentro do request do express tudo que o nosso middleware retornou.
+      next();
+    } else {
+      res
+        .status(httpResponse.statusCode)
+        .json({ error: httpResponse.body.message });
+    }
+  };
+};
