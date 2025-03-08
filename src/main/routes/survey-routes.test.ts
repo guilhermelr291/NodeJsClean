d9301f4482ede7e4a7430bc10d553f86fe5a5c9b
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-08 16:18:36 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -86,5 +86,60 @@ describe('Survey Routes', () => {
     test('Should return 403 on load surveys without accessToken', async () => {
       await request(app).get('/api/surveys').expect(403);
     });
+
+    test('Should return 200 on load surveys with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Guilherme',
+        email: 'guilhermelr@gmail.com',
+        password: '123',
+        role: 'admin',
+      });
+
+      const id = res.insertedId;
+      const accessToken = sign({ id }, env.jwtSecret);
+
+      await accountCollection.updateOne(
+        { _id: id },
+        {
+          $set: {
+            accessToken,
+          },
+        }
+      );
+
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer',
+            },
+            {
+              answer: 'another_answer',
+            },
+          ],
+          date: new Date(),
+        },
+        {
+          question: 'other_question',
+          answers: [
+            {
+              image: 'other_image',
+              answer: 'other_answer',
+            },
+            {
+              answer: 'other_answer',
+            },
+          ],
+          date: new Date(),
+        },
+      ]);
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200);
+    });
   });
 });
