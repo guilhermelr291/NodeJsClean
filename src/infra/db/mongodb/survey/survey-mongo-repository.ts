f215874fea477d8cb25a34ba16bf86e67ce03de6
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-08 15:41:05 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -8,10 +8,18 @@ import {
 } from '../../../../data/usecases/load-surveys/db-load-surveys-protocols';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository
+  implements AddSurveyRepository, LoadSurveysRepository
+{
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
 
     await surveyCollection.insertOne(surveyData);
+  }
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    let surveys: any = await surveyCollection.find().toArray();
+    surveys = surveys.map(item => MongoHelper.map(item));
+    return surveys;
   }
 }
