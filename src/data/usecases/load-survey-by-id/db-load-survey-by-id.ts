7d70ef8db3c2a43250ecdfe7c6c9eb8e0a601c23
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-12 18:24:45 -0300

    feat: ensure DbLoadSurveyById calls loadSurveyByIdRepository with correct id

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.ts
@@ -0,0 +1,16 @@
+import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id';
+import { SurveyModel } from '../load-surveys/db-load-surveys-protocols';
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository';
+
+export class DbLoadSurveyById implements LoadSurveyById {
+  constructor(
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {
+    this.loadSurveyByIdRepository = loadSurveyByIdRepository;
+  }
+  async loadById(id: string): Promise<SurveyModel> {
+    await this.loadSurveyByIdRepository.loadById(id);
+
+    return new Promise(resolve => resolve(null));
+  }
+}
