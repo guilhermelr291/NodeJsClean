cd59075fc7a1babe4d2d58ca02dffa2fff489012
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-12 22:23:42 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,61 @@
+import MockDate from 'mockdate';
+import {
+  DbSaveSurveyResult,
+  SaveSurveyResultModel,
+  SaveSurveyResultRepository,
+  SurveyResultModel,
+} from './db-save-survey-result-protocols';
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answer: 'any_answer',
+  date: new Date(),
+});
+
+const makeFakeSurveyResult = (): SurveyResultModel =>
+  Object.assign({}, makeFakeSurveyResultData(), {
+    id: 'any_id',
+  });
+
+type SutTypes = {
+  sut: DbSaveSurveyResult;
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository;
+};
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class saveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return new Promise(resolve => resolve(makeFakeSurveyResult()));
+    }
+  }
+  return new saveSurveyResultRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository();
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub);
+
+  return {
+    sut,
+    saveSurveyResultRepositoryStub: saveSurveyResultRepositoryStub,
+  };
+};
+
+describe('DbSaveSurveyResult Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test('Should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut();
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save');
+    const surveyResultData = makeFakeSurveyResultData();
+    await sut.save(surveyResultData);
+    expect(saveSpy).toHaveBeenCalledWith(surveyResultData);
+  });
+});
