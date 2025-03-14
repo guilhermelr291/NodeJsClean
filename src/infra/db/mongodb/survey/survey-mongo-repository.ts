26b5bc2a446b10f2087a7e471c4af2395e666b39
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-14 18:14:14 -0300

    feat: ensure SurveyMongoRepository return a survey by id

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -7,9 +7,11 @@ import {
   SurveyModel,
 } from '@/data/usecases/load-surveys/db-load-surveys-protocols';
 import { MongoHelper } from '../helpers/mongo-helper';
+import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id';
+import { ObjectId } from 'mongodb';
 
 export class SurveyMongoRepository
-  implements AddSurveyRepository, LoadSurveysRepository
+  implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyById
 {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
@@ -21,5 +23,13 @@ export class SurveyMongoRepository
     let surveys: any = await surveyCollection.find().toArray();
     surveys = surveys.map(item => MongoHelper.map(item));
     return surveys;
+  }
+  async loadById(_id: ObjectId): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const survey = await surveyCollection.findOne({
+      _id,
+    });
+
+    return MongoHelper.map(survey);
   }
 }
