c8b4fbade58afbf9daa728c8a6488d8217ca89c2
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-16 14:33:06 -0300

    test: ensure save survey result route returns 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest';
+import app from '../config/app';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL!);
+  });
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  //quando estamos mexendo com testes de integração e banco de dados,
+  // é importante também, entre os testes, zerarmos as respectivas tabelas.
+  //para evitar que fique lixo e influencie nos outros testes
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer',
+        })
+        .expect(403);
+    });
+  });
+});
