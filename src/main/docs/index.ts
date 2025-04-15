3c290c264e819e46d9afdffaf066af5328aceccc
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-04-15 16:55:06 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,32 @@
+import { loginPath } from './paths/login-path';
+import { accountSchema } from './schemas/account-schema';
+import { loginParamsSchema } from './schemas/login-params-schema';
+
 export default {
   openapi: '3.0.4',
   info: {
     title: 'Clean Node API',
     description: 'Clean Node API to make surveys',
     version: '1.0.0',
+  },
+
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+    },
+  ],
+  paths: {
+    '/login': loginPath,
+  },
+  components: {
+    schemas: {
+      account: accountSchema,
+      loginParams: loginParamsSchema,
+    },
   },
 };
---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,25 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API para autenticar usuário',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/loginParams',
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: { $ref: '#/components/schemas/account' },
+          },
+        },
+      },
+    },
+  },
+};
